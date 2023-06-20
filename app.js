9449a44cc5b35b20bbb1f389aed3a23e82ea998f
Ali <ali19961402@gmail.com>
2023-06-20 19:57:06 +0430

    first-commit

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const boxArray = Array.from(box);
 const h1=document.querySelector('h1')
 const player1 = document.querySelector(".player-1");
 const player2 = document.querySelector(".player-2");
+const newGame=document.querySelector('button.newGame');
 const buttonReset=document.querySelector('button[type="reset"]');
+const player=document.querySelector('span.player')
 const h2X=document.querySelector(".win-X");
 const h2O=document.querySelector(".win_O");
 let winX=0;
@@ -14,9 +16,11 @@ let currentPlayer = 1;
          boxArray[i].addEventListener("click", function () {
           if (!this.classList.contains("selected")) {
             if (currentPlayer == 1) {
+              player.innerText='player-O'
               this.innerHTML = '<span class="player-1">X</span>';
               currentPlayer = 2;
             } else {
+              player.innerText='player-X'
               this.innerHTML = '<span class="player-2">O</span>';
               currentPlayer = 1;
             }
@@ -34,48 +38,56 @@ let currentPlayer = 1;
               h1.innerText="win-X"
               winX=winX+1
               h2X.innerText=`X=${winX}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[3].innerHTML==='<span class="player-1">X</span>')&&(boxArray[4].innerHTML==='<span class="player-1">X</span>')&&(boxArray[5].innerHTML==='<span class="player-1">X</span>')){
               h1.innerText="win-X"
               winX=winX+1
               h2X.innerText=`X=${winX}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[6].innerHTML==='<span class="player-1">X</span>')&&(boxArray[7].innerHTML==='<span class="player-1">X</span>')&&(boxArray[8].innerHTML==='<span class="player-1">X</span>')){
               h1.innerText="win-X"
               winX=winX+1
               h2X.innerText=`X=${winX}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[0].innerHTML==='<span class="player-1">X</span>')&&(boxArray[3].innerHTML==='<span class="player-1">X</span>')&&(boxArray[6].innerHTML==='<span class="player-1">X</span>')){
               h1.innerText="win-X"
               winX=winX+1
               h2X.innerText=`X=${winX}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[1].innerHTML==='<span class="player-1">X</span>')&&(boxArray[4].innerHTML==='<span class="player-1">X</span>')&&(boxArray[7].innerHTML==='<span class="player-1">X</span>')){
               h1.innerText="win-X"
               winX=winX+1
               h2X.innerText=`X=${winX}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[2].innerHTML==='<span class="player-1">X</span>')&&(boxArray[5].innerHTML==='<span class="player-1">X</span>')&&(boxArray[8].innerHTML==='<span class="player-1">X</span>')){
               h1.innerText="win-X"
               winX=winX+1
               h2X.innerText=`X=${winX}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[0].innerHTML==='<span class="player-1">X</span>')&&(boxArray[4].innerHTML==='<span class="player-1">X</span>')&&(boxArray[8].innerHTML==='<span class="player-1">X</span>')){
               h1.innerText="win-X"
               winX=winX+1
               h2X.innerText=`X=${winX}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[2].innerHTML==='<span class="player-1">X</span>')&&(boxArray[4].innerHTML==='<span class="player-1">X</span>')&&(boxArray[6].innerHTML==='<span class="player-1">X</span>')){
               h1.innerText="win-X"
               winX=winX+1
               h2X.innerText=`X=${winX}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }
@@ -85,48 +97,56 @@ let currentPlayer = 1;
               h1.innerText="win-O"
               winO=winO+1
               h2O.innerText=`O=${winO}`
+              player.innerText='player-X'
               resetGame()  
               console.log("win");
             }else if((boxArray[3].innerHTML==='<span class="player-2">O</span>')&&(boxArray[4].innerHTML==='<span class="player-2">O</span>')&&(boxArray[5].innerHTML==='<span class="player-2">O</span>')){
               h1.innerText="win-O"
               winO=winO+1
               h2O.innerText=`O=${winO}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[6].innerHTML==='<span class="player-2">O</span>')&&(boxArray[7].innerHTML==='<span class="player-2">O</span>')&&(boxArray[8].innerHTML==='<span class="player-2">O</span>')){
               h1.innerText="win-O"
               winO=winO+1
               h2O.innerText=`O=${winO}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[0].innerHTML==='<span class="player-2">O</span>')&&(boxArray[3].innerHTML==='<span class="player-2">O</span>')&&(boxArray[6].innerHTML==='<span class="player-2">O</span>')){
               h1.innerText="win-O"
               winO=winO+1
               h2O.innerText=`O=${winO}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[1].innerHTML==='<span class="player-2">O</span>')&&(boxArray[4].innerHTML==='<span class="player-2">O</span>')&&(boxArray[7].innerHTML==='<span class="player-2">O</span>')){
               h1.innerText="win-O"
               winO=winO+1
               h2O.innerText=`O=${winO}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[2].innerHTML==='<span class="player-2">O</span>')&&(boxArray[5].innerHTML==='<span class="player-2">O</span>')&&(boxArray[8].innerHTML==='<span class="player-2">O</span>')){
               h1.innerText="win-O"
               winO=winO+1
               h2O.innerText=`O=${winO}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[0].innerHTML==='<span class="player-2">O</span>')&&(boxArray[4].innerHTML==='<span class="player-2">O</span>')&&(boxArray[8].innerHTML==='<span class="player-2">O</span>')){
               h1.innerText="win-O"
               winO=winO+1
               h2O.innerText=`O=${winO}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }else if((boxArray[2].innerHTML==='<span class="player-2">O</span>')&&(boxArray[4].innerHTML==='<span class="player-2">O</span>')&&(boxArray[6].innerHTML==='<span class="player-2">O</span>')){
               h1.innerText="win-O"
               winO=winO+1
               h2O.innerText=`O=${winO}`
+              player.innerText='player-X'
               resetGame()
               console.log("win");
             }
@@ -137,11 +157,21 @@ let currentPlayer = 1;
           boxArray[i].classList.remove("selected");
           setTimeout(() => {
             h1.innerText="win?"
-          },5000);
+          },1000);
         }
         currentPlayer = 1;
       }
 
-      buttonReset.addEventListener('click',function(){
+      newGame.addEventListener('click',function(){
+        player.innerText='player-X'
         resetGame()
       });
+
+      buttonReset.addEventListener('click',function(){
+        winX=0
+        h2X.innerText='X='
+        winO=0
+        h2O.innerText='O='
+        player.innerText='player-X'
+        resetGame()
+      })
